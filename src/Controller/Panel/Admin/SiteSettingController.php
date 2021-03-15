6fcf8189ce42903602e4ec949deffc595103c15e
HaciKandemir <hacikandemir2000@hotmail.com>
2021-03-16 02:34:11 +0300

    Terms admin panelinden yönetiliyor

--- src/Controller/Panel/Admin/SiteSettingController.php
+++ src/Controller/Panel/Admin/SiteSettingController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Panel\Admin;
 
 use App\Entity\Banner;
+use App\Entity\TermsOfUse;
 use App\Form\BannerType;
+use App\Form\TermsOfUseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,6 +36,33 @@ class SiteSettingController extends AbstractController
             );
         }
         return $this->render('panel/admin/site_setting/banner.html.twig',[
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/site-setting/privacy", name="admin_site_setting_privacy")
+     */
+    public function privacy(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $privacy = $em->getRepository(TermsOfUse::class)->find(1);
+
+        $form = $this->createForm(TermsOfUseType::class, $privacy);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $privacy = $form->getData();
+
+            $em->persist($privacy);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+        }
+        return $this->render('panel/admin/site_setting/privacy.html.twig',[
             "form" => $form->createView(),
         ]);
     }
