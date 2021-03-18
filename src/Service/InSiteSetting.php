785148916e4b062958bab04b8f5b6a7f299c7a5d
HaciKandemir <hacikandemir2000@hotmail.com>
2021-03-18 22:05:25 +0300

    admin kullanıcısı için url ler listelenirken oluşturan userin adı gösteriliyor

--- src/Service/InSiteSetting.php
+++ src/Service/InSiteSetting.php
@@ -6,6 +6,7 @@ use App\Entity\Banner;
 use App\Entity\Customers;
 use App\Entity\Features;
 use App\Entity\TermsOfUse;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class InSiteSetting extends AbstractController
@@ -32,5 +33,10 @@ class InSiteSetting extends AbstractController
     {
         $data = $this->getDoctrine()->getRepository(Customers::class)->findAll();
         return is_null($data) ? null : $data;
+    }
+
+    public function getUserName(int $id){
+        $data = $this->getDoctrine()->getRepository(User::class)->find($id);
+        return is_null($data) ? null : $data->getUserName();
     }
 }
